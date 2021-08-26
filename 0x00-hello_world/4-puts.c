f9045a8ed43edd943395a6043291bfdf002cb8ba
Mustapha Wolly <wollymustapha@gmail.com>
2021-08-25 23:24:53 -0400

    c file to print out a phrase using puts - betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+*main - This is the main function
+*
+*It always return 0 if the execution is a success 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
