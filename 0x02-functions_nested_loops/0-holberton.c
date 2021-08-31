db4496bb4b91be27419aa5bd547d6bd3da7e26f2
Mustapha Wolly <wollymustapha@gmail.com>
2021-08-31 08:46:25 -0400

    C file to print Holberton to screen using a function called _putchar.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints Holberton out on the screen.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *c = "Holberton";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
