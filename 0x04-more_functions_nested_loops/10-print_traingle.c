1c0772cee8e46d4ba09d66719814ae3ab8e3659c
Mustapha Wolly <wollymustapha@gmail.com>
2021-09-02 14:39:35 -0400

    function to print triangle using # character

--- 0x04-more_functions_nested_loops/10-print_traingle.c
+++ 0x04-more_functions_nested_loops/10-print_traingle.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle.
- * @size: numbers of lines.
- * Return: no return.
+ * print_triangle - Prints a triangle using the # character.
+ * @size: The size of the triangle.
  */
 void print_triangle(int size)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 1; j < (size - i); j++)
-			_putchar(' ');
-		for (j--; j < size; j++)
-			_putchar(35);
-		if (i < (size - 1))
-			_putchar('\n');
-	}
-	_putchar('\n');
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = size - i; j > 0; j--)
+_putchar(' ');
+
+for (j = 0; j < i; j++)
+_putchar('#');
+
+if (i == size)
+continue;
+
+_putchar('\n');
+}
+}
+
+_putchar('\n');
 }
