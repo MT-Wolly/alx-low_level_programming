ab27de3b16d218a5dbdce5608cbddf2948d14f25
Mustapha Wolly <wollymustapha@gmail.com>
2021-09-02 14:35:18 -0400

    function to print squares using # character

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,25 @@
-include "main.h"
+#include "main.h"
 
 /**
- * print_square - prints hash squares.
- * @size: size of the square.
- * Return: no return.
+ * print_square - Prints a square using the # character.
+ * @size: The size of the square.
  */
 void print_square(int size)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			_putchar(35);
-		}
-		if (i != size - 1)
-			_putchar('\n');
-	}
-	_putchar('\n');
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar('#');
+
+if (i == size - 1)
+continue;
+_putchar('\n');
+}
+}
+
+_putchar('\n');
 }
