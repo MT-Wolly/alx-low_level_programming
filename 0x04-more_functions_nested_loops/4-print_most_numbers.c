896e0c03e88b8132eeee88434179932335fd3d08
Mustapha Wolly <wollymustapha@gmail.com>
2021-09-01 21:25:24 -0400

    function to print most numbers with exception of 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * does not print 2 or 4.
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
