dc8ca811319bebb2344f050a62e689a1d25710a4
Mustapha Wolly <wollymustapha@gmail.com>
2021-09-06 11:13:23 -0400

    function to print array using pointer variables

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_array - prints elements of an array integer.
+* @a: input array
+* @n: number of integers
+* Return: no return
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (; i <= n; i++)
+	{
+		printf("%d", *(a + 1));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
