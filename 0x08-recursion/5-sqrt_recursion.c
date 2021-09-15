43b81fd4a7562e74d599859b9056fceb8a8ac09c
Mustapha Wolly <wollymustapha@gmail.com>
2021-09-14 23:18:22 -0400

    function to return natural squareroot of anumber

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* power - returns natural sqaure root of a number
+* @n: input integer
+* @p: squareroot operator
+* Return: squareroot of a number
+*/
+int power(int n, int p)
+{
+	if (p % (n / p) == 0)
+	{
+		if (p * (p / n) == n)
+			return (p);
+		else
+			return (-1);
+	}
+}
+/**
+* _sqrt_recursion - returns natural sqaure root of a number
+* @n: input integer
+* Return: squareroot of a number
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power(n, 2));
+}
