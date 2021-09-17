742554e666b2214809e48d61757fac2250d28f63
Mustapha Wolly <wollymustapha@gmail.com>
2021-09-16 20:05:44 -0400

    program to add two numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - adds two numbers
+* @argc: number of command line arguments
+* @argv: array containing command line arguments
+* Return: 0 if Success
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, addition;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		addition += atoi()argv[i];
+	}
+	printf("%d\n", addition);
+	return (0);
+}
